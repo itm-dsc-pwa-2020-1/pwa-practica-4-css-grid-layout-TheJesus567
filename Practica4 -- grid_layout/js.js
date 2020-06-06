06e4198b334e919ca29ff118101df8930acc5905
TheJesus567 <61330835+TheJesus567@users.noreply.github.com>
2020-06-06 15:32:26 -0500

    Add files via upload

---
+++ Practica4 -- grid_layout/js.js
@@ -0,0 +1,31 @@
+function dark() {
+  document.getElementById("fondo").style.background = "black";
+  document.getElementById("poft").style.color = "white";
+  document.getElementById("titulo").style.color = "white";
+  document.getElementById("titulo2").style.color = "white";
+  document.getElementById("titulop").style.color = "white";
+  document.getElementById("titulop1").style.color = "white";
+  document.getElementById("titulop2").style.color = "white";
+  document.getElementById("titulop3").style.color = "white";
+  document.getElementById("titulop4").style.color = "white";
+  document.getElementById("titulop5").style.color = "white";
+  document.getElementById("link").style.color = "white";
+  document.getElementById("linea").style.borderBottom = "thick solid #fff";
+  document.getElementById("linea2").style.borderTop = "thick solid #fff";
+}
+function normal() {
+  document.getElementById("fondo").style.background = "white";
+  document.getElementById("poft").style.color = "black";
+  document.getElementById("titulo").style.color = "black";
+  document.getElementById("titulo2").style.color = "black";
+  document.getElementById("titulop").style.color = "black";
+  document.getElementById("titulop1").style.color = "black";
+  document.getElementById("titulop2").style.color = "black";
+  document.getElementById("titulop3").style.color = "black";
+  document.getElementById("titulop4").style.color = "black";
+  document.getElementById("titulop5").style.color = "black";
+  document.getElementById("link").style.color = "black";
+  document.getElementById("linea").style.borderBottom = "thick solid #000";
+  document.getElementById("linea2").style.borderTop = "thick solid #000";
+
+}
